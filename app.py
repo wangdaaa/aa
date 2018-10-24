6366d04ebd18e2754aff95c9b4351b2aaeeaff78
wangdaaa <512022123@qq.com>
2018-10-24 22:53:13 +0800

    aa

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    print('sdfasdf')
+    print('这是更新程序vvv的')
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
